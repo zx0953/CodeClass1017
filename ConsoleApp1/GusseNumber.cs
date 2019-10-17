cab6acbaa24d35e82a10e90d6d3021a4d96fe4a0
zx0953 <lovesky0953zx@gmail.com>
2019-10-17 20:01:57 +0800

    Frist

---
+++ ConsoleApp1/GusseNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GusseNumber
+    {
+        int guess = -1;
+        int intputNum = 0;
+        
+        public void gusseNumPlay()
+        {
+            guess = new Random().Next(100);
+            while (true)
+            {
+                string inputStr = Console.ReadLine();
+                int intputNum = Int32.Parse(inputStr);
+                if(guess == intputNum)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("猜錯了");
+                }
+            }
+            Console.WriteLine("猜對了");
+            Console.ReadKey();
+        }
+    }
+}
