a90d98fd045b8509527aa01018dc29a2c17ae27b
zx0953 <lovesky0953zx@gmail.com>
2019-10-24 18:23:32 +0800

    完整 猜數字

--- ConsoleApp1/GusseNumber.cs
+++ ConsoleApp1/GusseNumber.cs
@@ -16,12 +16,14 @@ namespace ConsoleApp1
             
             try
             {
-                guess = new Random().Next(50);
+                guess = new Random().Next(9);
+                Console.WriteLine("0~9 猜一個數字");
                 while (true)
                 {
                     string inputStr = Console.ReadLine();
                     int intputNum = Int32.Parse(inputStr);
-                    if (guess == intputNum)
+                    Int32.TryParse(inputStr,out intputNum);
+                    if (guess == intputNum) 
                     {
                         break;
                     }
@@ -30,12 +32,13 @@ namespace ConsoleApp1
                         Console.WriteLine("猜錯了");
                     }
                 }
-                Console.WriteLine("猜對了");
+                Console.WriteLine("恭喜 恭喜 猜對了");
                 Console.ReadKey();
 
             }
             catch(Exception exp)
             {
+
                 Console.WriteLine(exp);
                 throw;
             }
